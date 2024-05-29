2afd3e1a026d5b06b416a0ee41d980667bd711b3
myersben9 <120344142+myersben9@users.noreply.github.com>
2024-05-29 13:14:41 -0700

    v3 bump

--- src/utils.py
+++ src/utils.py
@@ -10,9 +10,9 @@ import base64
 from fastapi import HTTPException, Request, Security, status
 from fastapi.security import APIKeyHeader
 from fastapi import FastAPI
-from noco import get_nocodb_data, get_nocodb_icons, SITE_HOST, API_KEYS, HTTP
+from src.noco import get_nocodb_data, get_nocodb_icons, SITE_HOST, API_KEYS, HTTP
 from logger import logger
-from models import OrderInfo
+from src.models import OrderInfo
 from tempfile import TemporaryDirectory
 import asyncio
 import requests
