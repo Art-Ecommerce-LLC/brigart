8bc4e4b50fe6b29ec78deef2f71d6b16a748dcef
myersben9 <120344142+myersben9@users.noreply.github.com>
2024-06-09 11:31:49 -0700

    v8

--- src/static/general_scripts.js
+++ src/static/general_scripts.js
@@ -1,8 +1,8 @@
 function toggleIcon() {
     const icon = document.querySelector('#nav-icon3');
     icon.classList.toggle('open');
-
 }
+
 function emailListEnter() {
     // Get the email input value
     const emailInput = document.getElementById('emailInput');
@@ -94,56 +94,149 @@ function updateCartQuantity() {
             console.error('Error:', error);
         });
 }
-//Create a second toggle for the mobile menu to be a dropdown menu when hamburger icon is clicked
+
 function toggleDropdown() {
     const dropdown = document.querySelector('.mobile-dropdown');
+    const glance_h1 = document.querySelector('.glance_inner h1');
     const content = document.querySelector('.content');
     const footer = document.querySelector('.footer');
     // Toggle the 'show' class to control visibility
     dropdown.classList.toggle('show');
     content.classList.toggle('hide');
     footer.classList.toggle('hide'); // Add or remove 'hide' class to hide or show the content
-
     // Lock the page from scrolling when the dropdown is open
     if (dropdown.classList.contains('show')) {
         document.body.style.overflow = 'hidden';
+        glance_h1.style.display = 'none'
     } else {
         document.body.style.overflow = 'auto';
+        glance_h1.style.display = 'block'
+        content.classList.remove('hide');
+        footer.classList.remove('hide');
     }
-}
 
+}
 function toggleMenu() {
     const navbar = document.querySelector('.navbar');
     const mobileMenu = document.querySelector('.mobile-menu');
+    const dropdown = document.querySelector('.mobile-dropdown')
+    const glance_h1 = document.querySelector('.glance_inner h1');
     const content = document.querySelector('.content');
     const footer = document.querySelector('.footer');
-    const dropdown = document.querySelector('.mobile-dropdown');
+    // Toggle the 'show' class to control visibility
+
     // Toggle the mobile menu
     if (window.innerWidth <= 827) {
         navbar.style.display = 'none';
         mobileMenu.style.display = 'flex';
         document.body.style.overflow = 'auto';
         if (dropdown.classList.contains('show')) {
+            document.body.style.overflow = 'hidden';
+            glance_h1.style.display = 'none';
             content.classList.add('hide');
             footer.classList.add('hide');
+            
         }
-        
+        else {
+            document.body.style.overflow = 'auto';
+            glance_h1.style.display = 'block';
+            content.classList.remove('hide');
+            footer.classList.remove('hide');
+        }
+
     } else {
         navbar.style.display = 'flex';
         mobileMenu.style.display = 'none';
         document.body.style.overflow = 'auto';
+        glance_h1.style.display = 'block'
         content.classList.remove('hide');
         footer.classList.remove('hide');
     }
+
+
 }
 
 // Initial toggle when the page loads
 document.addEventListener('DOMContentLoaded', function() {
     updateCartQuantity();
     toggleMenu();
+
+    // Check on scroll
+    window.addEventListener('resize', toggleMenu);
     document.getElementById('currentYear').innerText = new Date().getFullYear();
+
 });
 
 // Toggle the menu on window resize
-window.addEventListener('resize', toggleMenu);
 
+// Load images when the page is loaded
+document.addEventListener('DOMContentLoaded', function() {
+
+    loadImagesInView();
+    window.addEventListener('load', function() {
+        checkFade();
+    });
+
+    const artworkImages = document.querySelectorAll('img.artwork[style]');
+
+    artworkImages.forEach(img => {
+        const tempVar = img.getAttribute('style');
+        const preloadedImg = new Image();
+        preloadedImg.src = tempVar;
+
+        preloadedImg.onload = function() {
+            img.src = preloadedImg.src;
+        };
+    });
+
+
+});
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function togglePageLock(responsePromise) {
+    setButtonsState(true);
+    document.body.style.opacity = '0.5';
+    const spinner = document.createElement('div');
+    spinner.classList.add('spinner');
+    document.body.appendChild(spinner);
+
+    try {
+        const response = await responsePromise;
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        
+        return response
+    } finally {
+        // await sleep(2000)
+        // Unlock the buttons and the page
+        
+        document.body.style.opacity = '1';
+        spinner.remove();
+    }
+}
+function setButtonsState(disabled) {
+    const buttons = document.querySelectorAll('.increase-quantity, .decrease-quantity, .remove-item'); // Adjust the selector to match your button classes
+    // Can you also disable any links on the page
+    const links = document.querySelectorAll('a');
+    links.forEach(link => link.disabled = disabled);
+    buttons.forEach(button => button.disabled = disabled);
+}
+
+
+
+// Load images when scrolling
+window.addEventListener('scroll', function() {
+    loadImagesInView();
+    checkFade();
+});
+
+// Load images when resizing the window
+window.addEventListener('resize', function() {
+    loadImagesInView();
+    checkFade();
+});
+// Initial check on page load
