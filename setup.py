1b1ee8cad8a88ccd0847daef5d15324b2415b9d8
myersben9 <myersben9@outlook.com>
2024-07-18 20:18:58 -0700

    websockets

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ setup(
         'pillow==8.3.2',
         'aiohttp==3.9.5',
         'loguru==0.7.2',
-        'slowapi==0.1.9'
+        'slowapi==0.1.9',
+        'websockets==12.0'
     ],
     entry_points={
         'console_scripts': [
